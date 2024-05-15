5c30acf1832f44480af33f0864883a4ea86c1325
Roulof <Roelo.school@gmail.com>
2024-05-15 12:23:21 +0200

    Git aangemaakt. controllers en exceptions mappen aangemaakt. TelivisionsController.java klaargezet

---
+++ src/main/java/com/roel/backendles11/Backendles11Application.java
@@ -0,0 +1,13 @@
+package com.roel.backendles11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendles11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendles11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/roel/backendles11/Backendles11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roel.backendles11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendles11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
